6dd087d4ebdcca11d94997e07535f389e373ea84
ayana190 <TTC22D3170008@edu.tech.ac.jp>
2023-04-28 18:31:08 +0900

    Game
    papiermache

---
+++ Assets/Kojin Folder/Takahashi/Script/CardScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CardScript : MonoBehaviour
+{
+    // ボタンが押された場合、今回呼び出される関数
+    public void OnClick()
+    {
+        Debug.Log("カードです");  // ログを出力
+    }
+}
